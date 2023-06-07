a03f21a750191ba809c31c8e81befe9127dd3fc6
Adipebrian <pebrianadi05@gmail.com>
2023-06-07 22:30:03 +0700

    up

--- vendor/myth/auth/src/Views/login.php
+++ vendor/myth/auth/src/Views/login.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>PT. Andalan Prima Indonesia</title>
+	<title>SIPPOLSUB</title>
 	<link rel="stylesheet" href="<?= base_url() ?>/adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
 	<link rel="stylesheet" href="<?= base_url() ?>/assets/css/layout.css">
 	<link rel="stylesheet" href="<?= base_url() ?>/assets/css/responsive.css">
